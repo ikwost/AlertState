52b52a3e3890a9020c61fa53550add952a73f0ce
Eli O <ikwost@gmail.com>
2022-09-18 14:15:19 +0300

    new dependencies

--- app/src/main/java/com/ikwost/alertstate/MainActivity.kt
+++ app/src/main/java/com/ikwost/alertstate/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ikwost.alertstate.ui.theme.AlertStateTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    Text(text = "hi")
                 }
             }
         }
