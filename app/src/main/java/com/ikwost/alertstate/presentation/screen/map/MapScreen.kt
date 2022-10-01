51dcf52b9710888cfe8e3d6c4df1118965b3ea16
Eli O <ikwost@gmail.com>
2022-10-01 23:05:01 +0300

    map button check

--- app/src/main/java/com/ikwost/alertstate/presentation/screen/map/MapScreen.kt
+++ app/src/main/java/com/ikwost/alertstate/presentation/screen/map/MapScreen.kt
@@ -1,18 +1,28 @@
 package com.ikwost.alertstate.presentation.screen.map
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import com.ikwost.alertstate.navigation.Screen
 
 @Composable
 fun MapScreen(navController: NavHostController) {
     Scaffold {
-        Text(
-            text = "Hello Map",
+        Column {
+            Text(
+                text = "Hello Map",
 
-            )
+                )
+            Button(
+                onClick = { navController.navigate(route = Screen.Profile.route) }) {
+
+            }
+        }
+
     }
 }
